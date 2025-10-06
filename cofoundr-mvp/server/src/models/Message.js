db19ea9a0b1b9752ba77486edc5e9370a80bcecb
cofoundr1 <rajivdudhnath@gmail.com>
2025-10-06 04:21:31 -0400

    Delete cofoundr-mvp directory

--- cofoundr-mvp/server/src/models/Message.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-const MessageSchema = new mongoose.Schema({
-  from: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  to: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  content: { type: String, required: true },
-  read: { type: Boolean, default: false },
-}, { timestamps: true });
-export default mongoose.model('Message', MessageSchema);
--- cofoundr-mvp/web/src/socket.js
+++
@@ -1,2 +0,0 @@
-import { io } from 'socket.io-client';
-export const makeSocket = (token) => io(import.meta.env.VITE_SOCKET_URL, { auth: { token } });
